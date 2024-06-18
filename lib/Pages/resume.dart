0a5fa7ce34d778bbae1ab08e5c121d22b3d70232
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-06-18 19:40:20 +0300

    fix resume view

--- lib/Pages/resume.dart
+++ lib/Pages/resume.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:url_launcher/link.dart';
 
 class Resume extends StatelessWidget {
   const Resume({super.key});
@@ -11,9 +14,35 @@ class Resume extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Resume'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () => Get.back(),
+          ),
+          actions: [
+            Link(
+              target: LinkTarget.blank,
+              uri: Uri.parse(
+                'https://github.com/MH0386/MH0386/blob/main/resume.pdf',
+              ),
+              builder: (context, followLink) => IconButton(
+                icon: const FaIcon(FontAwesomeIcons.github),
+                onPressed: followLink,
+              ),
+            ),
+            Link(
+              target: LinkTarget.blank,
+              uri: Uri.parse(
+                'https://github.com/MH0386/MH0386/raw/main/resume.pdf',
+              ),
+              builder: (context, followLink) => IconButton(
+                icon: const FaIcon(FontAwesomeIcons.download),
+                onPressed: followLink,
+              ),
+            ),
+          ],
         ),
         body: SfPdfViewer.network(
-          'https://github.com/MH0386/MH0386/blob/main/resume.pdf',
+          'https://mh0386.github.io/resume.pdf',
         ),
       ),
     );
