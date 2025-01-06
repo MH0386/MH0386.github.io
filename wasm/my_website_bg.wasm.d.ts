ccd8dd10269c4280b3d01090efbecf8ae7171b80
MH0386 <mohamed.hisham.abdelzaher@gmail.com>
2025-01-06 12:06:14 +0000

    Deploying to gh-pages from @ MH0386/MH0386.github.io@6b6939cdc3fdb2ebc641f4dce09ee2b430e434c4 🚀

---
+++ wasm/my_website_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wbg_jsowner_free: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_4: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_export_7: WebAssembly.Table;
+export const _ZN132__LT_dyn_u20_core__ops__function__FnMut_LT__LP__RP__GT__u2b_Output_u20__u3d__u20_R_u20_as_u20_wasm_bindgen__closure__WasmClosure_GT_8describe6invoke17h302e0c54f2e8b8edE: (a: number, b: number) => void;
+export const closure200_externref_shim: (a: number, b: number, c: any) => void;
+export const closure204_externref_shim: (a: number, b: number, c: any) => void;
+export const closure494_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
