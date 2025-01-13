e727deab75d09f48e9c67cb7ff87a8b9e5bc1325
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-01-13 17:59:44 +0200

    Update devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -48,43 +48,8 @@
   # pre-commit.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
-  android = {
+  languages.rust = {
     enable = true;
-    flutter = {
-      enable = true;
-      package = pkgs.flutterPackages-source.beta;
-    };
-    platforms.version = [
-      "32"
-      "34"
-    ];
-    systemImageTypes = [ "google_apis_playstore" ];
-    abis = [
-      "arm64-v8a"
-      "x86_64"
-    ];
-    # cmakeVersions = [ "3.30.5" ];
-    cmdLineTools.version = "11.0";
-    tools.version = "26.1.1";
-    platformTools.version = "34.0.5";
-    buildTools.version = [ "30.0.3" ];
-    emulator = {
-      enable = true;
-      version = "34.1.9";
-    };
-    sources.enable = false;
-    systemImages.enable = true;
-    ndk.enable = true;
-    googleAPIs.enable = true;
-    extras = [ "extras;google;gcm" ];
-    extraLicenses = [
-      "android-sdk-preview-license"
-      "android-googletv-license"
-      "android-sdk-arm-dbt-license"
-      "google-gdk-license"
-      "intel-android-extra-license"
-      "intel-android-sysimage-license"
-      "mips-android-sysimage-license"
-    ];
+    channel = "stable";
   };
 }
