142f5725c33a3e3b3e9326e0de93024ae6457ef1
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-02-04 00:16:41 +0200

    Remove Tailwind CSS configuration and clean up manifest and CSS files

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use dioxus::prelude::*;
 use dioxus_router::prelude::*;
 
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 #[derive(Debug, Clone, Routable, PartialEq)]
 enum Route {
@@ -29,7 +28,6 @@ fn App() -> Element {
             href: "https://avatars.githubusercontent.com/u/77013511?v=4",
         }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
         Router::<Route> {}
     }
 }
@@ -37,10 +35,10 @@ fn App() -> Element {
 #[component]
 fn NavBar() -> Element {
     rsx! {
-        div { id: "navbar", class: "flex justify-between items-center p-4",
+        div { id: "navbar",
             Link { to: Route::Home {}, "Home" }
             Link { to: Route::About {}, "About" }
-            Link { to: Route::Projects {}, "Project" }
+            Link { to: Route::Projects {}, "Projects" }
             Link { to: Route::Certificates {}, "Certificates" }
             Link { to: Route::Contact {}, "Contact" }
             Link { to: Route::Resume {}, "Resume" }
@@ -52,12 +50,10 @@ fn NavBar() -> Element {
 #[component]
 fn Home() -> Element {
     rsx! {
-        section {
-            id: "myname",
-            class: "flex flex-col items-center justify-center text-xl h-screen",
-            p { class: "animate-fadeIn", "Hi, My name is" }
-            h1 { class: "font-black text-5xl animate-fadeIn", "Mohamed Hisham Abdelzaher" }
-            p { class: "animate-fadeIn", "Welcome to my personal website! (Alpha)" }
+        section { id: "myname_section",
+            p { "Hi, My name is" }
+            h1 { "Mohamed Hisham Abdelzaher" }
+            p { "Welcome to my personal website! (Alpha)" }
         }
     }
 }
@@ -66,9 +62,7 @@ fn Home() -> Element {
 #[component]
 fn About() -> Element {
     rsx! {
-        div {
-            id: "about",
-            class: "flex flex-col items-center justify-center text-xl h-screen",
+        section { id: "about",
             h2 { "About Me" }
             p { "I am an AI engineer with a passion for Rust." }
         }
@@ -78,9 +72,7 @@ fn About() -> Element {
 #[component]
 fn Projects() -> Element {
     rsx! {
-        section {
-            id: "projects",
-            class: "flex flex-col items-center justify-center text-xl h-screen",
+        section { id: "projects",
             h2 { "Projects" }
             p { "Here are some of my projects." }
         }
@@ -90,68 +82,53 @@ fn Projects() -> Element {
 #[component]
 fn Certificates() -> Element {
     rsx! {
-        section {
-            id: "certificates",
-            class: "flex flex-col items-center justify-center text-xl", // h-screen",
+        section { id: "certificates",
             h2 { "Certificates" }
             p { "Here are some of my certificates." }
             br {}
-            div { class: "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6",
-                div { class: "max-w-sm rounded-lg overflow-hidden shadow-lg p-4 bg-grey-100 border-2 border-gray-700",
+            div { id: "certificates_list",
+                div { id: "certificates_item",
                     Link {
                         to: "https://kaggle.com/learn/certification/mh0386/python",
                         page: "_blank",
-                        h4 { class: "text-white font-bold text-xl mb-2", "Python" }
-                        p { class: "text-white text-base", "Issuing Organization: Kaggle" }
-                        p { class: "text-white text-base", "Issue Date: March, 2023" }
+                        h4 { "Python" }
+                        p { "Issuing Organization: Kaggle" }
+                        p { "Issue Date: March, 2023" }
                     }
                 }
-                div { class: "max-w-sm rounded-lg overflow-hidden shadow-lg p-4 bg-grey-100 border-2 border-gray-700",
+                div { id: "certificates_item",
                     Link {
                         to: "https://coursera.org/verify/specialization/3BRYQRFUD5C6",
                         page: "_blank",
-                        h4 { class: "text-white font-bold text-xl mb-2",
-                            "Machine Learning Specialization"
-                        }
-                        p { class: "text-white text-base",
-                            "Issuing Organization: Stanford University, DeepLearning.AI"
-                        }
-                        p { class: "text-white text-base", "Issue Date: June, 2023" }
+                        h4 { "Machine Learning Specialization" }
+                        p { "Issuing Organization: Stanford University, DeepLearning.AI" }
+                        p { "Issue Date: June, 2023" }
                     }
                     br {}
-                    div { class: "max-w-sm rounded-lg overflow-hidden shadow-lg p-4 bg-grey-100 border-2 border-gray-700",
+                    div { id: "certificates_item",
                         Link {
                             to: "https://coursera.org/verify/XX8THJA26UTS",
                             page: "_blank",
-                            h4 { class: "text-white font-bold text-xl mb-2",
-                                "Supervised Machine Learning: Regression and Classification"
-                            }
-                            // p { class: "text-white text-base", "Issuing Organization: Kaggle" }
-                            p { class: "text-white text-base", "Issue Date: February, 2023" }
+                            h4 { "Supervised Machine Learning: Regression and Classification" }
+                            p { "Issue Date: February, 2023" }
                         }
                     }
                     br {}
-                    div { class: "max-w-sm rounded-lg overflow-hidden shadow-lg p-4 bg-grey-100 border-2 border-gray-700",
+                    div { id: "certificates_item",
                         Link {
                             to: "https://coursera.org/verify/HM55XWLDYPA3",
                             page: "_blank",
-                            h4 { class: "text-white font-bold text-xl mb-2",
-                                "Advanced Learning Algorithms"
-                            }
-                            // p { class: "text-white text-base", "Issuing Organization: Kaggle" }
-                            p { class: "text-white text-base", "Issue Date: March, 2023" }
+                            h4 { "Advanced Learning Algorithms" }
+                            p { "Issue Date: March, 2023" }
                         }
                     }
                     br {}
-                    div { class: "max-w-sm rounded-lg overflow-hidden shadow-lg p-4 bg-grey-100 border-2 border-gray-700",
+                    div { id: "certificates_item",
                         Link {
                             to: "https://coursera.org/verify/B4NKPXD9UN9Z",
                             page: "_blank",
-                            h4 { class: "text-white font-bold text-xl mb-2",
-                                "Unsupervised Learning, Recommenders, Reinforcement Learning"
-                            }
-                            // p { class: "text-white text-base", "Issuing Organization: Kaggle" }
-                            p { class: "text-white text-base", "Issue Date: June, 2023" }
+                            h4 { "Unsupervised Learning, Recommenders, Reinforcement Learning" }
+                            p { "Issue Date: June, 2023" }
                         }
                     }
                 }
@@ -163,102 +140,127 @@ fn Certificates() -> Element {
 #[component]
 fn Contact() -> Element {
     rsx! {
-        section {
-            id: "contact",
-            class: "flex flex-col items-center justify-center text-xl h-screen",
-            h2 { class: "font-black text-5xl m-4", "Contact" }
+        section { id: "contact",
+            h2 { "Contact" }
             Link {
                 to: "mailto:mohamed.hisham.abdelzaher@gmail.com",
                 page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1313394640393957378/L0W5hykJ_400x400.jpg",
                     alt: "Gmail",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "mohamed.hisham.abdelzaher@gmail.com"
             }
             br {}
-            Link { to: "https://github.com/MH0386", page: "_blank",
+            Link {
+                to: "https://github.com/MH0386",
+                page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1633247750010830848/8zfRrYjA_400x400.png",
                     alt: "GitHub",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "GitHub @MH0386"
             }
-            Link { to: "https://gitlab.com/MH0386", page: "_blank",
+            Link {
+                to: "https://gitlab.com/MH0386",
+                page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1526219998741647362/8KKDKESj_400x400.jpg",
                     alt: "GitLab",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "GitLab @MH0386"
             }
-            Link { to: "https://www.linkedin.com/in/MH0386", page: "_blank",
+            Link {
+                to: "https://www.linkedin.com/in/MH0386",
+                page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1661161645857710081/6WtDIesg_400x400.png",
                     alt: "LinkedIn",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "LinkedIn @MH0386"
             }
             Link {
                 to: "https://qabilah.com/profile/MH0386/professional-profile",
                 page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1807856405522583552/1_rr1lKY_400x400.jpg",
                     alt: "Qabilah",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "Qabilah @MH0386"
             }
-            Link { to: "https://www.hf.co/MH0386", page: "_blank",
+            Link {
+                to: "https://www.hf.co/MH0386",
+                page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1348748676282388482/nr8ZuLBE_400x400.jpg",
                     alt: "Hugging Face",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "HuggingFace @MH0386"
             }
-            Link { to: "https://www.x.com/MH0386", page: "_blank",
+            Link {
+                to: "https://www.x.com/MH0386",
+                page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg",
                     alt: "X",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "X @MH0386"
             }
-            Link { to: "https://www.kaggle.com/MH0386", page: "_blank",
+            Link {
+                to: "https://www.kaggle.com/MH0386",
+                page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1573129499343978498/03a7wgfE_400x400.jpg",
                     alt: "kaggle",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "Kaggle @MH0386"
             }
             Link {
                 to: "https://ollama.com/mohamedhishamabdelzaher",
                 page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1874043413466931204/BRau4SQI_400x400.jpg",
                     alt: "ollama",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "Ollama @mohamedhishamabdelzaher"
             }
-            Link { to: "https://app.opensauced.pizza/u/MH0386", page: "_blank",
+            Link {
+                to: "https://app.opensauced.pizza/u/MH0386",
+                page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1588597214141509632/uo5sqfVs_400x400.png",
                     alt: "OpenSauced",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "OpenSauced @MH0386"
             }
-            Link { to: "https://www.freelancer.com/u/MH0386", page: "_blank",
+            Link {
+                to: "https://www.freelancer.com/u/MH0386",
+                page: "_blank",
+                id: "contact_item",
                 img {
                     src: "https://pbs.twimg.com/profile_images/1838452019700600833/TmFzf9uW_400x400.jpg",
                     alt: "Freelancer",
-                    class: "inline-block w-4 h-4 mr-2",
+                    id: "contact_icon",
                 }
                 "Freelancer @MH0386"
             }
@@ -272,7 +274,6 @@ fn Resume() -> Element {
         div {
             embed {
                 id: "resume",
-                class: "flex flex-col items-center justify-center text-xl h-screen",
                 src: "https://mh0386.github.io/data/docs/resume.pdf",
                 r#type: "application/pdf",
                 p {
