412149555acb069a8e77dbb749c94da465aff292
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-05-03 01:24:49 +0300

    chore: update dependencies and improve project configuration
    - Added mypy, black, and pyright to the development dependencies in pyproject.toml.
    - Updated Python version requirement in uv.lock to >=3.11.
    - Enhanced .idea configuration for Black and Mypy settings.
    - Refactored main.py to implement route handling and improve app structure.

--- src/main.py
+++ src/main.py
@@ -1,26 +1,50 @@
-import flet as ft
+import flet as ft  # type: ignore
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    page.title = "Routes Example"
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
+    def route_change(route: ft.RouteChangeEvent):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.AppBar(
+                        title=ft.Text("Flet app"),
+                        bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+                    ),
+                    ft.ElevatedButton(
+                        "Visit Store", on_click=lambda _: page.go("/store")
+                    ),
+                ],
+            )
         )
-    )
+        if page.route == "/store":
+            page.views.append(
+                ft.View(
+                    "/store",
+                    [
+                        ft.AppBar(
+                            title=ft.Text("Store"),
+                            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+                        ),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        page.update()
+        print(route)
+
+    def view_pop(view: ft.RouteChangeEvent):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(route=top_view.route)  # pyright: ignore [reportArgumentType]
+        print(view)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
 
 
-ft.app(main)
+ft.app(main, view=ft.AppView.WEB_BROWSER)
