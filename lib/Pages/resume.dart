442bd5b8c9e472039df69027caa256545aadbfc0
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-06-03 02:48:04 +0300

    fix pdfviewer

--- lib/Pages/resume.dart
+++ lib/Pages/resume.dart
@@ -3,21 +3,13 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:pdfrx/pdfrx.dart';
 import 'package:url_launcher/link.dart';
-import 'package:internet_file/internet_file.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Resume extends StatelessWidget {
   const Resume({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // final pdfPinchController = PdfControllerPinch(
-    //   document: PdfDocument.openData(
-    //     InternetFile.get(
-    //       'https://raw.githubusercontent.com/MH0386/MH0386/main/resume.pdf',
-    //     ),
-    //   ),
-    //   viewportFraction: 1.5,
-    // );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: Colors.black),
@@ -25,28 +17,13 @@ class Resume extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.black,
           foregroundColor: Colors.white,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(width: MediaQuery.of(context).size.width * 0.1),
-              const Text('Resume'),
-              SizedBox(width: MediaQuery.of(context).size.width * 0.1),
-            ],
-          ),
+          title: const Text('Resume'),
           centerTitle: true,
           leading: IconButton(
             icon: const Icon(Icons.arrow_back),
             onPressed: () => Get.back(),
           ),
           actions: [
-            Link(
-              target: LinkTarget.blank,
-              uri: Uri.parse('https://mh0386.github.io/resume.pdf'),
-              builder: (context, followLink) => IconButton(
-                icon: const FaIcon(FontAwesomeIcons.eye),
-                onPressed: followLink,
-              ),
-            ),
             Link(
               target: LinkTarget.blank,
               uri: Uri.parse(
@@ -60,7 +37,21 @@ class Resume extends StatelessWidget {
           ],
         ),
         body: PdfViewer.uri(
-          Uri.parse('https://raw.githubusercontent.com/MH0386/MH0386/main/resume.pdf'),
+          Uri.parse(
+            'https://raw.githubusercontent.com/MH0386/MH0386/main/resume.pdf',
+          ),
+          params: PdfViewerParams(
+            backgroundColor: Colors.black,
+            enableTextSelection: true,
+            enableKeyboardNavigation: true,
+            linkHandlerParams: PdfLinkHandlerParams(
+              onLinkTap: (link) {
+                if (link.url != null) {
+                  launchUrl(link.url!);
+                }
+              },
+            ),
+          ),
         ),
       ),
     );
