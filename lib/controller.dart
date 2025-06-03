a74590766a6c0d8fa4e230d235b1d4db5dee6c93
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-06-03 18:37:28 +0300

    rm @override

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,31 +1,23 @@
 import 'dart:async';
-
 import 'package:get/get.dart';
 
 class TextController extends GetxController {
   var currentIndex = 0.obs;
   var socialMediaTitle = "Social Media".obs;
-  List<String> welcomeTexts = [
-    "Welcome to My Portfolio",
-    "I am AI Engineer",
-  ];
+  List<String> welcomeTexts = ["Welcome to My Portfolio", "I am AI Engineer"];
 
-  @override
   Timer? _timer;
 
   @override
   void onInit() {
     super.onInit();
-    _timer = Timer.periodic(
-      const Duration(seconds: 5),
-      (timer) {
-        if (currentIndex.value < welcomeTexts.length - 1) {
-          currentIndex.value++;
-        } else {
-          currentIndex.value = 0;
-        }
-      },
-    );
+    _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      if (currentIndex.value < welcomeTexts.length - 1) {
+        currentIndex.value++;
+      } else {
+        currentIndex.value = 0;
+      }
+    });
   }
 
   @override
