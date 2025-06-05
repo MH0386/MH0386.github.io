01072c698813771d8be841e8122b4ee3697c504d
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2025-06-06 02:40:25 +0300

    refactor: clean up `contact.dart`, streamline UI structure, and improve button handling

--- lib/Pages/contact.dart
+++ lib/Pages/contact.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mh0386/connector.dart';
 import 'package:mh0386/info.dart';
 import 'package:mh0386/style.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class Contact extends StatelessWidget {
   const Contact({super.key});
-  List<ElevatedButton> _buildSocialMediaLink() {
+
+  List<ElevatedButton> _buildContactButtons() {
     return Info().socialMediaLinks.map((link) {
       return ElevatedButton(
-        onPressed: () => launchUrl(Uri.parse(link['url']!)),
+        onPressed: goToUrl(Uri.parse(link['url']!)),
         style: Style().buttonStyle,
         child: Text(
           link['name']!,
@@ -18,7 +19,7 @@ class Contact extends StatelessWidget {
       );
     }).toList()..add(
       ElevatedButton(
-        onPressed: () => launchUrl(Uri.parse('mailto:${Info().email}')),
+        onPressed: goToUrl(Uri.parse('mailto:${Info().email}')),
         style: Style().buttonStyle,
         child: Text(
           Info().email,
@@ -30,25 +31,22 @@ class Contact extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: Colors.black),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.white,
-          title: const Text('Contact'),
-          centerTitle: true,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () => Get.back(),
-          ),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: const Text('Contact'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Get.back(),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: _buildSocialMediaLink(),
-          ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _buildContactButtons(),
         ),
       ),
     );
